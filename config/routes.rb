b4b9f28be9090e0d0061ccb3ba6e0d51977c1788
nruest <ruestn@gmail.com>
2017-11-08 16:16:26 -0500

    oops

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  root 'pages#show", page: "home'
+  root 'pages#show', page: 'home'
   get '/pages/:page' => 'pages#show'
 
   get '/auth/:provider/callback', to: 'sessions#create'
