af065b62c79c53c35b5fbb7eeda90578374ea6d6
nruest <ruestn@gmail.com>
2017-11-10 13:00:26 -0500

    Add codecov.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,6 +3,10 @@
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
+require 'codecov'
+SimpleCov.formatter = SimpleCov::Formatter::Codecov
+
+# Test helper methods.
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in
   # alphabetical order.
