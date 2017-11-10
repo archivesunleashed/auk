b44c2c3dbf788adfed03618d573e2e9075319412
nruest <ruestn@gmail.com>
2017-11-09 20:42:14 -0500

    rubocop

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -5,9 +5,5 @@ OmniAuth.config.logger = Rails.logger
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
   provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET'],
-    {
-      :authorize_params =>{
-        :use_authorize => 'true'
-      }
-  }
+           authorize_params: { use_authorize: 'true' }
 end
