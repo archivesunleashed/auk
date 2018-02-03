be5aec1b8441a0f2e97a61173f4b5f3ef1629755
nruest <ruestn@gmail.com>
2018-02-03 17:17:56 -0500

    Redirect to user's profile instead of root.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_or_create_from_auth_hash(auth_hash)
     session[:user_id] = @user.id
-    redirect_to root_path
+    redirect_to @user
   end
 
   def destroy
