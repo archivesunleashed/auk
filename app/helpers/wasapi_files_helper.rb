72ab6ff35ca3013e60d0b6edd868fe25ad5e034f
nruest <ruestn@gmail.com>
2018-02-14 00:23:54 -0500

    Total disk usage for an account; Resolves #33.
    - Create helper for disk usage
    - DRY'd up sidebar in collection/user view

--- app/helpers/wasapi_files_helper.rb
+++ app/helpers/wasapi_files_helper.rb
@@ -10,4 +10,10 @@ module WasapiFilesHelper
     file_size = WasapiFile.where(collection_id: collection_id).sum(:size)
     number_to_human_size(file_size)
   end
+
+  def disk_usage(user_id)
+    account = WasapiFile.where(user_id: user_id).distinct.pluck(:account)
+    account_path = ENV['DOWNLOAD_PATH'] + '/' + account.first.to_s
+    number_to_human_size(`du -sb "#{account_path}"`.split("\t").first.to_i)
+  end
 end
