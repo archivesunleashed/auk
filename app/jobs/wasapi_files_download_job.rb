67a7f0faa381fe4d2aaf24f02675a112ec3ac27f
nruest <ruestn@gmail.com>
2018-02-22 12:16:02 -0500

    Add some additional logic on downloader job.
    - Checks to see if file exists
    - If file exists, checksum validation
    - If validation true, continue on to the next
    - If validation false, remove existing file, and re-download
    - If file does not exist, download file
    - Update rubocop config for additions to job
    - Alphabetize rubocop config
    - Add OS gem (future work coming)

--- app/jobs/wasapi_files_download_job.rb
+++ app/jobs/wasapi_files_download_job.rb
@@ -16,10 +16,30 @@ class WasapiFilesDownloadJob < ApplicationJob
                       '/' + wasapi_file.collection_id.to_s + '/' + 'warcs/'
       download_path_filename = download_path + wasapi_file.filename
       FileUtils.mkdir_p download_path
-      download = open(wasapi_file.location_archive_it,
-                      http_basic_authentication: [wasapi_username,
-                                                  wasapi_password])
-      IO.copy_stream(download, download_path_filename)
+      if File.exist?(download_path_filename)
+        check_file = Digest::MD5.file(download_path_filename).hexdigest
+        if check_file == wasapi_file.checksum_md5
+          logger.info '[INFO] File exists: ' + download_path_filename
+        end
+        if check_file != wasapi_file.checksum_md5
+          logger.info '[INFO] Checksum mismatch:' + download_path_filename
+          FileUtils.rm(download_path_filename)
+          logger.info '[INFO] Downloading: ' + wasapi_file.location_archive_it
+          download = open(wasapi_file.location_archive_it,
+                          http_basic_authentication: [wasapi_username,
+                                                      wasapi_password])
+          IO.copy_stream(download, download_path_filename)
+          logger.info '[INFO] Downloaded: ' + wasapi_file.location_archive_it
+        end
+      end
+      if !File.exist?(download_path_filename)
+        logger.info '[INFO] Downloading: ' + wasapi_file.location_archive_it
+        download = open(wasapi_file.location_archive_it,
+                        http_basic_authentication: [wasapi_username,
+                                                    wasapi_password])
+        IO.copy_stream(download, download_path_filename)
+        logger.info '[INFO] Downloaded: ' + wasapi_file.location_archive_it
+      end
     end
   end
 end
