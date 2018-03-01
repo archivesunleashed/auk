34b0d290880ec1d5448226bd437fecb471d2a826
nruest <ruestn@gmail.com>
2018-03-01 17:48:35 -0500

    Resolve #39 - tie collection_size and collection_count to user.
    - Adds user_id to query
    - update related views

--- app/helpers/wasapi_files_helper.rb
+++ app/helpers/wasapi_files_helper.rb
@@ -2,12 +2,13 @@
 
 # WasapiFiles helper methods.
 module WasapiFilesHelper
-  def collection_count(collection_id)
-    WasapiFile.where(collection_id: collection_id).count
+  def collection_count(collection_id, user_id)
+    WasapiFile.where(collection_id: collection_id, user_id: user_id).count
   end
 
-  def collection_size(collection_id)
-    file_size = WasapiFile.where(collection_id: collection_id).sum(:size)
+  def collection_size(collection_id, user_id)
+    file_size = WasapiFile.where(collection_id: collection_id,
+                                 user_id: user_id).sum(:size)
     number_to_human_size(file_size)
   end
 
