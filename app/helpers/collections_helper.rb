ba017e8dd4ec82e1c79abeb2fc5fc5f382f55b92
nruest <ruestn@gmail.com>
2018-03-02 14:07:48 -0500

    Address #32; create display_gexf helper.

--- app/helpers/collections_helper.rb
+++ app/helpers/collections_helper.rb
@@ -18,4 +18,14 @@ module CollectionsHelper
       end
     end
   end
+
+  def display_gexf(user_id, collection_id, account)
+    collection_path = ENV['DOWNLOAD_PATH'] +
+                      '/' + account.to_s +
+                      '/' + collection_id.to_s + '/'
+    gexf_file = collection_path + user_id.to_s +
+                '/derivatives/gephi/' + collection_id.to_s +
+                '-gephi.gexf'
+    gexf_file
+  end
 end
