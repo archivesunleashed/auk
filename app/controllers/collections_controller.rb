bc7b9fe5447bd98e19e40f133ee7a6401dfb15d3
nruest <ruestn@gmail.com>
2018-03-15 09:59:37 -0400

    Resolve #71; Need to know who @user is.

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -84,6 +84,7 @@ class CollectionsController < ApplicationController
   end
 
   def correct_user
+    @user = User.find(params[:user_id])
     redirect_to(root_url) unless current_user?(@user)
   end
 end
