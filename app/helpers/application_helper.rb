4863da8afc53303ddc94f3463a6c163ee0ae4522
nruest <ruestn@gmail.com>
2018-03-27 09:10:14 -0400

    Setup dynamic titles; resolves #83.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@
 
 # Application helper.
 module ApplicationHelper
+  def full_title(page_title = '')
+    base_title = 'Archives Unleashed'
+    if page_title.empty?
+      base_title
+    else
+      page_title + ' | ' + base_title
+    end
+  end
 end
