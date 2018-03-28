a978d372087f91ba6bb4d5b09d9099dcfe8bf078
greebie <ryan.deschamps@gmail.com>
2018-03-28 11:19:42 -0400

    Add js to assets instead of inline.

---
+++ app/assets/javascripts/graphs.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+  if (typeof $("#graph").data("gexf") != 'undefined') {
+    var gexfFileData = $("#graph").data("gexf");
+    create_graph(gexfFileData);
+  }
+});
+
+function create_graph(data) {
+  if (data != ``) {
+    data = $.parseXML(data);
+    var so = new sigma("graph");
+    sigma.parsers.gexf(
+      data,
+      so,
+    function(y) {
+      so.settings({
+        nodeColor: 'default',
+        edgeColor: 'default',
+      })
+      so.refresh()
+    }
+  );
+} else {
+  $("#graph").append("Cannot find Gexf file");
+}}
