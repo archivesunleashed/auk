b97704f437e74eb9f4d8e8cf7500634750a8296d
Nick Ruest <ruestn@gmail.com>
2018-03-28 08:51:05 -0400

    Gravatar test (#88)
    * Add users_helper_test
    
    - test gravatar_url
    - Update test fixtures for users
    - Update Rubocop config because asserting urls requires long line

---
+++ test/helpers/users_helper_test.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class UsersHelperTest < ActionView::TestCase
+  def setup
+    @user_one = users(:one)
+    @user_two = users(:two)
+  end
+
+  test 'gravatar url helper' do
+    assert_equal 'http://gravatar.com/avatar/8df8b5d5fccdafd0514fba299539b331.png?s=200', gravatar_url(@user_one.email, 200)
+    assert_equal 'http://gravatar.com/avatar/b7e7642a5afced788bd300f5cd22506e.png?s=200&d=https%3A%2F%2Fuser-images.githubusercontent.com%2F218561%2F35773409-62e82b86-091f-11e8-8dec-188fc80c846b.jpg', gravatar_url(@user_two.email, 200)
+  end
+end
