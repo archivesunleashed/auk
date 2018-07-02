e6e5524e153a5de7edaad5494f51a428b325aaad
Ryan Deschamps <ryan.deschamps@gmail.com>
2018-07-02 13:01:32 -0400

    Add Neighbours highlight on mouseover. (#144)
    * Add arrows to show direction of links.
    * Highlights a node's neighbours on hover. (Greys out non-neighbours).

--- app/assets/javascripts/graphs.js
+++ app/assets/javascripts/graphs.js
@@ -5,8 +5,18 @@ function createGraph(data, instance) {
       instance.settings({
         nodeColor: 'default',
         edgeColor: 'default',
+        defaultEdgeType: 'arrow',
         labelThreshold: 7,
-        minNodeSize: 3
+        minNodeSize: 3,
+        minArrowSize: 5
+      });
+      // We first need to save the original colors of our
+      // nodes and edges, like this:
+      instance.graph.nodes().forEach(function (n) {
+        n.originalColor = n.color;
+      });
+      instance.graph.edges().forEach(function (e) {
+        e.originalColor = e.color;
       });
       if (instance.graph.nodes().length === 0) {
         instance.graph.addNode({
@@ -128,24 +138,108 @@ function leaveFullScreen() {
 
 $(document).on('turbolinks:load', function () {
   var state = 0;
-  var so = new sigma({ renderers: [ // eslint-disable-line new-cap
+  var so;
+  var gm;
+  if (sigma && !sigma.classes.graph.hasMethod('neighbors')) {
+    // create neighbors method to access neighbors of a node.
+    sigma.classes.graph.addMethod('neighbors', function (nodeId) {
+      var neighbors = {};
+      var index = this.allNeighborsIndex[nodeId] || {};
+      var here = this;
+      Object.keys(index).forEach(function (key) {
+        neighbors[key] = here.nodesIndex[key];
+      });
+      return neighbors;
+    });
+  }
+  so = new sigma({ renderers: [ // eslint-disable-line new-cap
     {
       container: document.getElementById('graph'),
       type: 'canvas' // sigma.renderers.canvas works as well
     }]
   });
-  var gm = new sigma({ renderers: [ // eslint-disable-line new-cap
+  gm = new sigma({ renderers: [ // eslint-disable-line new-cap
     {
       container: document.getElementById('graph-modal'),
       type: 'canvas'
     }]
   });
+
   graphRender(so);
   graphRender(gm);
-
   // resize graph-modal if the window changes
   $(window).on('resize', function () {
     $('div#graph-modal').height($(window).height() * 0.83);
+  });
+
+  so.bind('overNode', function (node) {
+    var nodeId = node.data.node.id;
+    var toKeep = so.graph.neighbors(nodeId);
+    toKeep[nodeId] = node.data.node;
+    so.graph.nodes().forEach(function (n) {
+      if (toKeep[n.id]) {
+        n.color = n.originalColor;
+      } else {
+        n.color = 'rgba(200, 200, 200, 0.75)';
+      }
+    });
+    so.graph.edges().forEach(function (e) {
+      if (toKeep[e.source] && toKeep[e.target]) {
+        e.color = e.originalColor;
+      } else {
+        e.color = 'rgba(200, 200, 200, 0.75)';
+      }
+    });
+    setTimeout(function () {
+      so.refresh();
+    }, 200);
+  });
+
+  gm.bind('overNode', function (node) {
+    var nodeId = node.data.node.id;
+    var toKeep = gm.graph.neighbors(nodeId);
+    toKeep[nodeId] = node.data.node;
+    gm.graph.nodes().forEach(function (n) {
+      if (toKeep[n.id]) {
+        n.color = n.originalColor;
+      } else {
+        n.color = 'rgba(200, 200, 200, 0.75)';
+      }
+    });
+    gm.graph.edges().forEach(function (e) {
+      if (toKeep[e.source] && toKeep[e.target]) {
+        e.color = e.originalColor;
+      } else {
+        e.color = 'rgba(200, 200, 200, 0.75)';
+      }
+    });
+    setTimeout(function () {
+      gm.refresh();
+    }, 200);
+  });
+
+  so.bind('outNodes', function () {
+    so.graph.nodes().forEach(function (n) {
+      n.color = n.originalColor;
+    });
+    so.graph.edges().forEach(function (e) {
+      e.color = e.originalColor;
+    });
+    setTimeout(function () {
+      so.refresh();
+    }, 200);
+  });
+
+  gm.bind('outNodes', function () {
+    gm.graph.nodes().forEach(function (n) {
+      n.color = n.originalColor;
+    });
+    gm.graph.edges().forEach(function (e) {
+      e.color = e.originalColor;
+    });
+    setTimeout(function () {
+      gm.refresh();
+    }, 2000);
   });
 
   $('.zoom-in').on('click', function () {
