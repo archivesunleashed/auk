c0d5c57553064e51f5c341dac841fa3867f7e225
nruest <ruestn@gmail.com>
2018-08-08 15:11:00 -0400

    Dashboards Controller test; resolves #152.

--- test/controllers/dashboards_controller_test.rb
+++ test/controllers/dashboards_controller_test.rb
@@ -7,4 +7,13 @@ class DashboardsControllerTest < ActionDispatch::IntegrationTest
     @dashboard_one = dashboards(:one)
     @dashboard_two = dashboards(:two)
   end
+
+  test 'get dashboards index' do
+    params = { test: 'object' }
+    auth_headers = { "Authorization" => "Basic #{Base64.encode64('test:test')}" }
+    get dashboards_path, params: params, as: :html
+    assert_response 401
+    get dashboards_path, params: params, as: :html, headers: auth_headers
+    assert_response :success
+  end
 end
