d1817e6a43efc3d10d7a168c22377d032d107988
nruest <ruestn@gmail.com>
2018-08-15 12:55:34 -0400

    Update sitemap to add derivative tutorials; #148.

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -5,4 +5,9 @@ SitemapGenerator::Sitemap.compress = false
 SitemapGenerator::Sitemap.create do
   add '/about', changefreq: 'monthly', priority: 0.9
   add '/documentation', changefreq: 'monthly', priority: 0.9
+  add '/derivatives', changefreq: 'monthly', priority: 0.9
+  add '/derivatives/gephi', changefreq: 'monthly', priority: 0.9
+  add '/derivatives/text-antconc', changefreq: 'monthly', priority: 0.9
+  add '/derivatives/text-sentiment', changefreq: 'monthly', priority: 0.9
+  add '/derivatives/domains', changefreq: 'monthly', priority: 0.9
 end
