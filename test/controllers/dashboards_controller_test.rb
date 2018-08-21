a184b4d10edb20ef584d4c44af3b249ab43c7e4b
nruest <ruestn@gmail.com>
2018-08-21 08:24:50 -0400

    Test Dashboard Controller params; resolves #154.

--- test/controllers/dashboards_controller_test.rb
+++ test/controllers/dashboards_controller_test.rb
@@ -9,11 +9,16 @@ class DashboardsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'get dashboards index' do
-    params = { test: 'object' }
+    params = { dashboard: {
+      job_id: '1abas', user_id: '298486374', collection_id: '1234',
+      queue: 'download', start_time: '2018-08-01 08:48:01',
+      end_time: '2018-08-12 06:18:01'
+    } }
     auth_headers = { "Authorization" => "Basic #{Base64.encode64('test:test')}" }
     get dashboards_path, params: params, as: :html
     assert_response 401
     get dashboards_path, params: params, as: :html, headers: auth_headers
     assert_response :success
+    assert_select 'td', 'Sample Collection'
   end
 end
