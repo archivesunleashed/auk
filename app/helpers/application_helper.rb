f862c00c9f3e7abfc100c773ad6f1f2234a220ed
nruest <ruestn@gmail.com>
2018-08-22 12:43:09 -0400

    Update sidbar nav with dashboard buttons; resolves #177.
    - Add helper method to get jobs run for a user
    - Update sidebar nav to use buttons for
      - disk usage
      - jobs run
    - Add test for new helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,4 +14,8 @@ module ApplicationHelper
   def archiveit_collection_url(collection_id)
     'https://archive-it.org/collections/' + collection_id.to_s
   end
+
+  def user_jobs_run(user_id)
+    Dashboard.where('user_id = ? AND queue = ?', user_id, 'cleanup').count
+  end
 end
--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -6,6 +6,8 @@ class ApplicationHelperTest < ActionView::TestCase
   def setup
     @collections_one = collections(:one)
     @collections_two = collections(:two)
+    @dashboards_one = dashboards(:one)
+    @dashboards_two = dashboards(:two)
   end
 
   test 'get archive-it collection url' do
@@ -13,5 +15,10 @@ class ApplicationHelperTest < ActionView::TestCase
                  archiveit_collection_url(@collections_one.collection_id)
     assert_equal 'https://archive-it.org/collections/3490',
                  archiveit_collection_url(@collections_two.collection_id)
+  end
+
+  test 'get amount of jobs a user has run' do
+    assert_equal 0, user_jobs_run(@dashboards_one.user_id)
+    assert_equal 0, user_jobs_run(@dashboards_two.user_id)
   end
 end
