e2ba4a1f9b4cf25b8c9eaba5771c7bcd38dc5031
Nick Ruest <ruestn@gmail.com>
2019-01-24 11:02:26 -0500

    Add test for disk_usage; resolves #159. (#252)
    - Update DOWNLOAD_PATH variable
    - Update rubocop config
    - Add test fixture directory for DOWNLOAD_PATH

--- test/helpers/dashboards_helper_test.rb
+++ test/helpers/dashboards_helper_test.rb
@@ -4,7 +4,7 @@ require 'test_helper'
 
 class DashboardsHelperTest < ActionView::TestCase
   def setup
-    ENV['DOWNLOAD_PATH'] = '/tmp'
+    ENV['DOWNLOAD_PATH'] = 'test/fixtures/files'
     @dashboards_one = dashboards(:one)
     @dashboards_two = dashboards(:two)
   end
