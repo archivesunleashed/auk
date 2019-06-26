57aa5af7ce1a0f80ab1cc9827cb785d9379620d6
nruest <ruestn@gmail.com>
2019-06-26 08:24:07 -0400

    Add spark.kryoserializer.buffer.max to spark job.
    - Sort spark conf variables in spark job
    - Sort variables in application config example

--- app/jobs/spark_job.rb
+++ app/jobs/spark_job.rb
@@ -32,15 +32,16 @@ class SparkJob < ApplicationJob
       FileUtils.mkdir_p collection_derivatives
       FileUtils.mkdir_p collection_spark_jobs_path
       FileUtils.mkdir_p collection_derivatives + '/gephi'
+      aut_version = ENV['AUT_VERSION']
+      spark_driver_max_result_size = ENV['SPARK_DRIVER_MAXRESULTSIZE']
+      spark_heartbeat_interval = ENV['SPARK_HEARTBEAT_INTERVAL']
+      spark_kryoserializer_buffer_max = ENV['SPARK_KRYOSERIALIZER_BUFFER_MAX']
       spark_memory_driver = ENV['SPARK_MEMORY_DRIVER']
       spark_network_timeout = ENV['SPARK_NETWORK_TIMEOUT']
-      aut_version = ENV['AUT_VERSION']
-      spark_threads = ENV['SPARK_THREADS']
-      spark_heartbeat_interval = ENV['SPARK_HEARTBEAT_INTERVAL']
-      spark_driver_max_result_size = ENV['SPARK_DRIVER_MAXRESULTSIZE']
       spark_rdd_compress = ENV['SPARK_RDD_COMPRESS']
       spark_serializer = ENV['SPARK_SERIALIZER']
       spark_shuffle_compress = ENV['SPARK_SHUFFLE_COMPRESS']
+      spark_threads = ENV['SPARK_THREADS']
       spark_job = %(
       import io.archivesunleashed._
       import io.archivesunleashed.app._
@@ -73,7 +74,7 @@ class SparkJob < ApplicationJob
       sys.exit
       )
       File.open(collection_spark_job_file, 'w') { |file| file.write(spark_job) }
-      spark_job_cmd = spark_shell + ' --master local[' + spark_threads + '] --driver-memory ' + spark_memory_driver + ' --conf spark.network.timeout=' + spark_network_timeout + ' --conf spark.executor.heartbeatInterval=' + spark_heartbeat_interval + ' --conf spark.driver.maxResultSize=' + spark_driver_max_result_size + ' --conf spark.rdd.compress=' + spark_rdd_compress + ' --conf spark.serializer=' + spark_serializer + '  --conf spark.shuffle.compress=' + spark_shuffle_compress + ' --packages "io.archivesunleashed:aut:' + aut_version + '" -i ' + collection_spark_job_file + ' 2>&1 | tee ' + collection_spark_job_file + '.log'
+      spark_job_cmd = spark_shell + ' --master local[' + spark_threads + '] --driver-memory ' + spark_memory_driver + ' --conf spark.network.timeout=' + spark_network_timeout + ' --conf spark.executor.heartbeatInterval=' + spark_heartbeat_interval + ' --conf spark.driver.maxResultSize=' + spark_driver_max_result_size + ' --conf spark.rdd.compress=' + spark_rdd_compress + ' --conf spark.serializer=' + spark_serializer + '  --conf spark.shuffle.compress=' + spark_shuffle_compress + ' --conf spark.kryoserializer.buffer.max=' + spark_kryoserializer_buffer_max + ' --packages "io.archivesunleashed:aut:' + aut_version + '" -i ' + collection_spark_job_file + ' 2>&1 | tee ' + collection_spark_job_file + '.log'
       logger.info 'Executing: ' + spark_job_cmd
       system(spark_job_cmd)
       domain_success = collection_derivatives + '/all-domains/output/_SUCCESS'
