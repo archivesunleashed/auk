6debe89f4f1313998ae15e06baf792b18754af94
nruest <ruestn@gmail.com>
2019-12-11 16:46:43 -0500

    Bump job length since we have gone well past thirty! :sweat:

--- config/initializers/delayed_job_config.rb
+++ config/initializers/delayed_job_config.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-Delayed::Worker.max_run_time = 30.days
+Delayed::Worker.max_run_time = 90.days
 Delayed::Worker.logger = LogStashLogger.new(
   type: :file, path: 'log/logstash_delayed_job.log', sync: true
 )
