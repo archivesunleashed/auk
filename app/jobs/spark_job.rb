f6b07c2a79cf895f33a706d9796d3bd02c24968d
nruest <ruestn@gmail.com>
2020-04-14 12:25:50 -0400

    Tooooooooooooooooo many escapes.
    - See: https://github.com/archivesunleashed/aut/commit/96899f46898d3cba7a8080a593d28f7b692320e4

--- app/jobs/spark_job.rb
+++ app/jobs/spark_job.rb
@@ -65,8 +65,8 @@ class SparkJob < ApplicationJob
       // Gephi GraphML.
       val links = warcs.map(r => (r.getCrawlDate, ExtractLinksRDD(r.getUrl, r.getContentString)))
         .flatMap(r => r._2.map(f => (r._1,
-                               ExtractDomainRDD(f._1).replaceAll("^\\\\s*www\\\\.", ""),
-                               ExtractDomainRDD(f._2).replaceAll("^\\\\s*www\\\\.", ""))))
+                               ExtractDomainRDD(f._1).replaceAll("^\\s*www\\.", ""),
+                               ExtractDomainRDD(f._2).replaceAll("^\\s*www\\.", ""))))
         .filter(r => r._2 != "" && r._3 != "")
         .countItems()
         .filter(r => r._2 > 5)
