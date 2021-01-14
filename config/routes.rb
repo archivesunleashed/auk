f31f62bbade9da64b447abcfe2b816d26f9d135a
ianmilligan1 <i2milligan@uwaterloo.ca>
2021-01-13 22:41:00 -0500

    Cloud warning and page, resolves #457

--- config/routes.rb
+++ config/routes.rb
@@ -51,6 +51,7 @@ Rails.application.routes.draw do
   root 'pages#show', page: 'home'
   get '/pages/:page' => 'pages#show'
   get 'about' => 'pages#about'
+  get 'archive-it' => 'pages#archive-it'
   get 'documentation' => 'pages#documentation'
   get 'faq' => 'pages#faq'
   get 'privacypolicy' => 'pages#privacypolicy'
